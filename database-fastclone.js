6806f442b999e94a3a7aa99b57887b3a0525b347
Fríðbjartur Miðberg <fm@Fribjarturs-MacBook-Pro.local>
2019-03-05 12:49:00 +0100

    Changed DB link

--- database-fastclone.js
+++ database-fastclone.js
@@ -2,7 +2,6 @@
 
 window.addEventListener("DOMContentLoaded", init);
 
-
 function init() {
   console.log("init");
   get();
@@ -10,15 +9,15 @@ function init() {
 
 // sends a get request to the db
 function post(newAlbum) {
-  fetch("https://musicdb-a19d.restdb.io/rest/albums", {
-      method: "post",
-      body: JSON.stringify(newAlbum),
-      headers: {
-        "Content-Type": "application/json; charset=utf-8",
-        "x-apikey": "5c7cf047cac6621685acbaf3",
-        "cache-control": "no-cache"
-      }
-    })
+  fetch("https://test01-0e26.restdb.io/rest/albums", {
+    method: "post",
+    body: JSON.stringify(newAlbum),
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": "5c7e37f2cac6621685acbb47",
+      "cache-control": "no-cache"
+    }
+  })
     .then(res => res.json())
     .then(data => {
       showAlbum(data);
@@ -29,14 +28,14 @@ function post(newAlbum) {
 function get() {
   console.log("get");
   //   use database endpoint with security code to get posts
-  fetch("https://musicdb-a19d.restdb.io/rest/albums", {
-      method: "get",
-      headers: {
-        "Content-Type": "application/json; charset=utf-8",
-        "x-apikey": "5c7cf047cac6621685acbaf3",
-        "cache-control": "no-cache"
-      }
-    })
+  fetch("https://test01-0e26.restdb.io/rest/albums", {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": "5c7e37f2cac6621685acbb47",
+      "cache-control": "no-cache"
+    }
+  })
     //   format as jason
     .then(res => res.json())
     .then(data => {
@@ -48,18 +47,16 @@ function get() {
 
 // sends a delete request to the db
 function deleteAlbum(id) {
-  fetch("https://musicdb-a19d.restdb.io/rest/albums/" + id, {
-      method: "delete",
-      headers: {
-        "Content-Type": "application/json; charset=utf-8",
-        "x-apikey": "5c7cf047cac6621685acbaf3",
-        "cache-control": "no-cache"
-      }
-    })
+  fetch("https://test01-0e26.restdb.io/rest/albums" + id, {
+    method: "delete",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": "5c7e37f2cac6621685acbb47",
+      "cache-control": "no-cache"
+    }
+  })
     .then(res => res.json())
-    .then(data => {
-
-    });
+    .then(data => {});
 }
 
 // loop to add posts to DOM - template -> clone -> append
@@ -71,22 +68,21 @@ function showAlbum(album) {
   clone.querySelector("[data-title]").textContent = album.Title;
   clone.querySelector("[data-year]").textContent = album.Year;
   clone.querySelector("[data-id]").dataset.id = album._id;
-  clone.querySelector("button").addEventListener("click", (e) => {
+  clone.querySelector("button").addEventListener("click", e => {
     // mouseevnt on click
     console.log(e);
     // finds mouse target (button) and delete its parent element on click (article)
     e.target.parentElement.remove();
     deleteAlbum(album._id);
-  })
+  });
   document.querySelector("[data-container]").appendChild(clone);
 }
 
 document.querySelector("button").addEventListener("click", () => {
-
   const obj = {
     Artist: "Madonna",
     Title: "I'm not a virgin",
     Year: 1995
-  }
+  };
   post(obj);
-})
+});
